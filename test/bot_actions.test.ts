a1e639f2e65a34bbf17a7d4503e00f256c1d048a
Tzahi Furmanski <tzahi.fur@gmail.com>
2020-09-29 23:34:27 +0300

    chore/rename test file and add a small test

--- test/bot_actions.test.ts
+++ test/bot_actions.test.ts
@@ -1,12 +1,25 @@
 // TODO: Write the tests, they are just stubs ATM
-test("app_mention - invalid command", () => {
-  expect(2).toBe(2);
-});
+import { handle_event } from "../src/bot_actions";
+import { introduce_yourself_action } from "../src/actions/introduce_yourself";
 
-test("app_mention - compliment command", () => {
-  expect(2).toBe(2);
-});
+describe("handle_event", () => {
+  test("Command that matches", () => {
+    // TODO: Need to implement a mock here
+    // jest.mock("../src/actions/introduce_yourself");
+    //
+    // introduce_yourself_action.mockClear();
+    //
+    // expect(2).toBe(2);
+  });
 
-test("app_mention - introduce yourself command", () => {
-  expect(2).toBe(2);
+  test("Command that does not match", () => {
+    console.log = jest.fn();
+
+    const event = {
+      text: "Super Weird Command that does not match anything :(",
+    };
+
+    handle_event(event);
+    expect(console.log).toHaveBeenCalledWith("Unsupported event", event);
+  });
 });
