9cda491d30836745a7d93b6bdcc0b79083fbc09c
Tzahi Furmanski <tzahi.fur@gmail.com>
2022-05-05 23:58:07 +0300

    Chore: Refactor ask channel stats functionality to be dynamic so won't need 4 commands for it.

--- src/actions/asks/index.ts
+++ src/actions/asks/index.ts
@@ -2,11 +2,8 @@ import { BotAction } from "../base_action";
 import { Compliment } from "./compliment";
 import { IntroduceYourself } from "./introduce_yourself";
 import { WhatCanYouDo } from "./what_can_you_do";
-import { AskChannelSummaryStats } from "./ask_channel_stats";
+import { AskChannelStats } from "./ask_channel_stats";
 import { MeaningOfLife } from "./meaning_of_life";
-import { AskChannelWeeklyStats } from "./ask_channel_weekly_stats";
-import { AskChannelDailyStats } from "./ask_channel_daily_stats";
-import { AskChannelMonthlyStats } from "./ask_channel_monthly_stats";
 import { GroupAskChannelMonthlyStats } from "./group_ask_channel_monthly_stats";
 import { MonitoredChannelSummaryStats } from "./monitored_channel_stats";
 import { AskChannelStatsForYesterday } from "./ask_channel_stats_for_yesterday";
@@ -20,10 +17,7 @@ export const ASKS_ACTIONS: BotAction[] = [
   new WhatCanYouDo(),
   new GroupAskChannelMonthlyStats(),
   new AskChannelStatsForYesterday(),
-  new AskChannelSummaryStats(),
-  new AskChannelDailyStats(),
-  new AskChannelWeeklyStats(),
-  new AskChannelMonthlyStats(),
+  new AskChannelStats(),
   new MeaningOfLife()
 ];
 
