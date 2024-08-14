8207f92bd66fe92e813d7b39d5ddf8197f2266b3
Tzahi Furmanski <tzahi.fur@gmail.com>
2024-08-14 12:38:55 +0300

    Chore: Add logging and error handling + Update dependencies (#125)
    * Chore: Improve logging and add error handling to the AskChannelStatusForYesterday function
    
    * Chore: Update libraries

--- src/integrations/slack/events_listeners/reaction_added.ts
+++ src/integrations/slack/events_listeners/reaction_added.ts
@@ -19,11 +19,13 @@ const reactionAddedCallback = async ({ event, client }: any) => {
 
     const team = findTeamByValue(event.item.channel, "ask_channel_id");
     if (!team) {
-      logger.error("Could not find team for ask channel", event.item.channel);
+      logger.error(
+        `Unable to find team for channel ID ${event.item.channel}. Ask: ${event.text}`
+      );
 
       return;
     }
-    
+
     // TODO: Maybe unneeded - Could it be that the bot is adding emojis?
     if (isBotMessage(event)) {
       logger.debug("Got a message from bot, skipping.");
@@ -38,7 +40,7 @@ const reactionAddedCallback = async ({ event, client }: any) => {
       event.item.ts,
       event.item.ts,
       1,
-      true,
+      true
     );
     if (messages.length == 0) {
       // Cant find original message,
